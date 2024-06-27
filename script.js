43e31c4930e57fbc3c09918ecd8c791783ecb741
RamiroAguerre <164430745+RamiroAguerre@users.noreply.github.com>
2024-06-26 22:27:39 -0300

    s

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-    // Función para ocultar la imagen
     $('#ocultarImagen').click(function() {
         $('#imagen').hide();
         $('#tituloPagina').text('¡No tenemos ninguna imagen!');
     });
 
-    // Función para mostrar la imagen
     $('#mostrarImagen').click(function() {
         $('#imagen').show();
         $('#tituloPagina').text('¡Mirá la siguiente imagen!');
